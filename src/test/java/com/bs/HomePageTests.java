00550fb10ff335703a443c36656385878e5fe957
sahilapte <sahilapte14@gmail.com>
2025-10-11 20:54:54 +0530

    pushing HomePageTests

--- src/test/java/com/bs/HomePageTests.java
+++ src/test/java/com/bs/HomePageTests.java
@@ -1,173 +1,107 @@
 package com.bs;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HomePageTests extends  BaseTest {
+import java.time.Duration;
 
-    @FindBy(xpath = "//a[@class='Navbar_logo__26S5Y']//*[name()='svg']")
-    public WebElement browserStackDemoLogo;
+public class HomePageTests extends BaseTest {
 
-    @FindBy(xpath = "//strong[normalize-space()='Offers']")
-    public WebElement offersLink;
+    private WebDriverWait wait;
 
-    @FindBy(xpath = "//strong[normalize-space()='Orders']")
-    public WebElement ordersLink;
+    /**
+     * TC-183: End to End Flow Test
+     */
+    @Test
+    public void endToEndFLow() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        voidLogin();
+        addToCartAndCheckout();
+        enterShippingDetails();
+        String msg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[normalize-space()='Your Order has been successfully placed.']"))).getText();
+        Assert.assertEquals(msg, "Your Order has been successfully placed.");
+    }
 
-    @FindBy(xpath = "//strong[normalize-space()='Favourites']")
-    public WebElement favouritesLink;
+    /**
+     * TC-181: Method to validate Shipping details mandatory fields
+     */
+    @Test
+    public void validateShippingDetailsMandatoryFields() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        voidLogin();
+        addToCartAndCheckout();
+        type(By.xpath("//input[@id='firstNameInput']"), generateRandomString());
+        type(By.xpath("//input[@id='lastNameInput']"), generateRandomString());
+        String urlBeforeSubmit=driver.getCurrentUrl();
+        clickOnElement(By.xpath("//button[@id='checkout-shipping-continue']"));
+        String urlAfterSubmit=driver.getCurrentUrl();
+        Assert.assertEquals(urlBeforeSubmit,urlAfterSubmit);
+    }
 
-    @FindBy(xpath = "//small[@class=\"products-found\"]//span")
-    public WebElement productsCount;
+    /* Helper methods*/
 
-    @FindBy(xpath = "//span[@class='bag bag--float-cart-closed']")
-    public WebElement cartIcon;
+    public void voidLogin() {
+        try {
+            driver.get("https://testathon.live/");
+            clickOnElement(By.xpath("//a[@id='Sign In']"));
+            clickOnElement(By.id("username"));
+            Actions actions = new Actions(driver);
+            actions.sendKeys(Keys.ARROW_DOWN, Keys.ENTER).perform();
+            clickOnElement(By.id("password"));
+            actions.sendKeys(Keys.ARROW_DOWN, Keys.ENTER).perform();
+            clickOnElement(By.id("login-btn"));
+        } catch (Exception e) {
+            Assert.fail("Login failed: " + e.getMessage());
+        }
+    }
 
-    @FindBy(xpath = "//div[@id='1']//div[@class='shelf-item__buy-btn'][normalize-space()='Add to cart']")
-    public WebElement addToCartButton;
+    public void addToCartAndCheckout() {
+        clickOnElement(By.xpath("//div[@id='1']//div[@class='shelf-item__buy-btn' and normalize-space()='Add to cart']"));
+        clickOnElement(By.xpath("//div[@class='buy-btn']"));
+    }
 
-    @FindBy(xpath ="//div[@data-sku=\"iPhone12-device-info.png\" and @id=\"3\"]//button")
-    public WebElement getFavouritesLink;
+    public void enterShippingDetails() {
+        try {
+            type(By.xpath("//input[@id='firstNameInput']"), generateRandomString());
+            type(By.xpath("//input[@id='lastNameInput']"), generateRandomString());
+            type(By.xpath("//input[@id='addressLine1Input']"), generateRandomString());
+            type(By.xpath("//input[@id='provinceInput']"), generateRandomString());
+            type(By.xpath("//input[@id='postCodeInput']"), generateRandomString());
+            clickOnElement(By.xpath("//button[@id='checkout-shipping-continue']"));
+        } catch (Exception e) {
+            Assert.fail("Entering shipping details failed: " + e.getMessage());
+        }
+    }
 
-    @FindBy(xpath = "//div[@class='buy-btn']")
-    public WebElement checkout;
+    public void type(By by, CharSequence data) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by)).sendKeys(data);
+    }
 
+    public void clickOnElement(By by) {
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
 
-    @FindBy(id = "username")
-    public WebElement usernameDropdown;
-
-    @FindBy(id = "password")
-    public WebElement passwordDropdown;
-
-    @FindBy(id="login-btn")
-    public WebElement loginButton;
-
-    @FindBy(xpath ="//input[@id='firstNameInput']")
-    public WebElement firstNameInput;
-
-    @FindBy(xpath ="///input[@id='lastNameInput']")
-    public WebElement lastNameInput;
-
-    @FindBy(xpath = "//input[@id='addressLine1Input']")
-    public WebElement addressLine1Input;
-
-    @FindBy(xpath = "//input[@id='addressLine1Input']")
-    public  WebElement addressLine2Input;
-
-    @FindBy(xpath = "//input[@id='postCodeInput']")
-    public WebElement postCodeInput;
-
-    @FindBy(xpath = "//button[@id='checkout-shipping-continue']")
-    public WebElement continueButton;
-
-    @FindBy(xpath = "//button[@id='checkout-shipping-continue']")
-    public WebElement orderSuccessMessage;
-
-    @FindBy(xpath = "//button[normalize-space()='Continue Shopping »']")
-    public WebElement continueShoppingButton;
-
-    @FindBy(xpath = "//button[@id='checkout-shipping-continue']")
-    public WebElement submit;
-
-
-    @FindBy(xpath = "//a[@id='logout']")
-    public WebElement logoutButton;
-
-    @FindBy(xpath = "//a[@id='Sign In']")
-    public WebElement signIn;
+    public String getTextMessage(By by) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(by)).getText().trim();
+    }
 
     public String generateRandomString() {
-        int length = 5;
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder randomString = new StringBuilder();
+        try{
+            int length = 5;
         for (int i = 0; i < length; i++) {
             int index = (int) (Math.random() * characters.length());
             randomString.append(characters.charAt(index));
+        }}
+        catch (Exception e) {
+            Assert.fail("Random string generation failed: " + e.getMessage());
         }
         return randomString.toString();
     }
-
-    public void voidLogin() {
-        driver.get("https://testathon.live/");
-
-
-//        signIn.click();
-        ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("arguments[0].click();", signIn);
-        usernameDropdown.click();
-        usernameDropdown.sendKeys(Keys.RETURN);
-
-        passwordDropdown.click();
-        passwordDropdown.sendKeys(Keys.RETURN);
-
-        loginButton.click();
-    }
-    public void addtoCartAndCheckout(){
-        voidLogin();
-        addToCartButton.click();
-        checkout.click();
-    }
-
-    public void enterShippingDetails(){
-        firstNameInput.sendKeys(generateRandomString());
-        lastNameInput.sendKeys(generateRandomString());
-        addressLine1Input.sendKeys(generateRandomString());
-        addressLine2Input.sendKeys(generateRandomString());
-        postCodeInput.sendKeys(generateRandomString());
-        submit.click();
-    }
-
-    @Test
-    public void endTOEndFLow() throws InterruptedException{
-        voidLogin();
-        addtoCartAndCheckout();
-        enterShippingDetails();
-        orderSuccessMessage.isDisplayed();
-        Assert.assertEquals(orderSuccessMessage.getText(),"Your Order has been successfully placed.");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
